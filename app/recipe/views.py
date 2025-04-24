0a399c64a5f3a9a9b8c7206f063456e3f813df75
Đào Bình <125723352+dabi61@users.noreply.github.com>
2025-04-24 20:20:51 +0700

    Update views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -44,7 +44,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             '-id')  # Lọc các recipe chỉ thuộc về người dùng hiện tại
 
     def get_serializer_class(self):
-    """Return the serializer class for request."""
+        """Return the serializer class for request."""
     if self.action == 'list':
         return serializers.RecipeSerializer
     elif self.action == 'retrieve':
