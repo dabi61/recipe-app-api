aad234b384df5b35b1dd3b06c9bf07eac7bc2dbe
Đào Bình <125723352+dabi61@users.noreply.github.com>
2025-04-24 20:29:15 +0700

    Update views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,16 +1,14 @@
-"""
-View for the recipe APIs
-"""
+"""View for the recipe APIs"""
+
 # Là một viewset của DRF tự động cung cấp các hành động
 from rest_framework import (
     viewsets,
     mixins,
+    status,
 )
-from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
-# Phương thức xác thực bằng token, Client phải gửi token trong header để
-# xác thực
+# Phương thức xác thực bằng token, Client phải gửi token trong header để xác thực
 from rest_framework.authentication import TokenAuthentication
 # Quyền hạn yêu cầu người dùng phải đăng nhập
 from rest_framework.permissions import IsAuthenticated
@@ -23,25 +21,23 @@ from core.models import (
 from recipe import serializers
 
 # VD về model viewset
-# GET /recipes/ Lấy danh sách recipe
-# POST /recipes/: Tạo 1 recipe mới
-# GET /recipes/id: Lấy 1 recipe chi tiết
-# PUT /recipes/id : update một recipe
-# DELETE /recipes/id Delete 1 recipe
+# GET /recipes/          -> Lấy danh sách recipe
+# POST /recipes/         -> Tạo 1 recipe mới
+# GET /recipes/{id}/     -> Lấy 1 recipe chi tiết
+# PUT /recipes/{id}/     -> Cập nhật 1 recipe
+# DELETE /recipes/{id}/  -> Xóa 1 recipe
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs"""
     serializer_class = serializers.RecipeDetailSerializer  # convert data
     queryset = Recipe.objects.all()  # getAll data
-    # Chỉ định phương thức xác thực (mỗi request đi kèm với 1 token)
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]  # Phải đăng nhập mới vào đuộc
+    authentication_classes = [TokenAuthentication]  # Xác thực bằng token
+    permission_classes = [IsAuthenticated]  # Phải đăng nhập mới truy cập được
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by(
-            '-id')  # Lọc các recipe chỉ thuộc về người dùng hiện tại
+        return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
@@ -52,7 +48,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         elif self.action == 'upload_image':
             return serializers.RecipeImageSerializer
         return self.serializer_class
-
 
     def perform_create(self, serializer):
         """Create a new recipe"""
@@ -67,13 +62,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
-                            mixins.UpdateModelMixin, 
-                            mixins.ListModelMixin, 
-                            viewsets.GenericViewSet):
+
+class BaseRecipeAttrViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
     """Base viewset for recipe attribute"""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -82,7 +80,8 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
-# Mixin những class nhỏ cho phép CRUD nhanh 
+
+# Mixin những class nhỏ cho phép CRUD nhanh
 class TagViewSet(BaseRecipeAttrViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
@@ -93,5 +92,3 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database."""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    
-    
