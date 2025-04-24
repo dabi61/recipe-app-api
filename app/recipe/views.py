006381a307186a6c002e0309155b5b2c63d59abd
Đào Bình <125723352+dabi61@users.noreply.github.com>
2025-04-24 20:22:55 +0700

    Update views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -45,13 +45,13 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
-    if self.action == 'list':
-        return serializers.RecipeSerializer
-    elif self.action == 'retrieve':
-        return serializers.RecipeDetailSerializer  # ✅ show chi tiết thì dùng detail
-    elif self.action == 'upload_image':
-        return serializers.RecipeImageSerializer
-    return self.serializer_class
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        elif self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer  # ✅ show chi tiết thì dùng detail
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
+        return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe"""
